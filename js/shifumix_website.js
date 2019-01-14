15fb5cd12f206b6985e48384a7fb9705fe6e8f80
shifumix <dev@shifumix.com>
2019-01-14 17:30:53 +0100

    avec les faq version 0.1

--- js/shifumix_website.js
+++ js/shifumix_website.js
@@ -7,7 +7,7 @@ function setprofil(i) {
     if (type_profil == 'perso') document.getElementById("services_pro").innerHTML="";
     if (type_profil == 'pro') document.getElementById("services_perso").innerHTML = "";
 
-    for(var i=0;i<10;i++){
+    for(var i=0;i<15;i++){
         var elt=document.getElementById("slide"+i);
         if(elt!=null && elt.tag!=null && type_profil.indexOf(elt.tag)==-1)
             document.removeChild(elt);
@@ -30,6 +30,7 @@ function openScreen(template){
         if(template==2)url="https://goo.gl/QWSsQT";
         if(template==3)url="https://goo.gl/uDzjt9";
         if(template==8)url="https://goo.gl/zB4R8s";
+        if(template==9)url="https://goo.gl/zB4R8s";
     } else {
         if(template==1)url="https://goo.gl/giM8HG";
         if(template==2)url="http://localhost:8080/widgets/freeScreen.html?eventcode=557394&sc=1547055085317&widget=musicOnly&event=event1547055085317&user=event_owner&message=Cette%20%C3%A9cran%20vous%20permet%20de%20partager%20votre%20%C3%A9v%C3%A9nement%20avec%20l%27ensemble%20des%20participants&1547055086050";
