708055523b56622f335f0495ab4d56b2fa0e201e
shifumix <dev@shifumix.com>
2019-01-14 17:34:46 +0100

    avec les faq version 0.1

--- js/shifumix_website.js
+++ js/shifumix_website.js
@@ -26,11 +26,11 @@ function openTemplates(){
 function openScreen(template){
     var url="";
     if(document.location.href.indexOf("127.0.0.1")==-1 && document.location.href.indexOf("localhost")==-1){
-        if(template==1)url="https://goo.gl/o4UUTq";
-        if(template==2)url="https://goo.gl/QWSsQT";
-        if(template==3)url="https://goo.gl/uDzjt9";
-        if(template==8)url="https://goo.gl/zB4R8s";
-        if(template==9)url="https://goo.gl/zB4R8s";
+        if(template==1)url="https://shifumixweb.appspot.com/template.html?duration=480&autologin=2&title=Eren%20Titan%20Form%20photo%20party&widgets=moderngallery&public=false&noprompt=true&autorun=1&dest=undefined&ihm=perso&activities=photo&flyer=https://fthmb.tqn.com/wKkkWbIJxFrKrJ75cxSk2UgiRdw=/2119x1415/filters:fill(auto,1)/iStock-618066222-59a42d02685fbe00102b159a.jpg";
+        if(template==2)url="https://shifumixweb.appspot.com/template.html?duration=480&autologin=2&title=Eren%20Titan%20Form%20party&widgets=musicOnly&public=false&noprompt=true&autorun=1&dest=undefined&ihm=perso&activities=music,message,photo&flyer=https://www.thebostoncalendar.com/system/events/photos/000/026/871/original/Party_Dream_Color_party.jpg?1443946618";
+        if(template==3)url="https://shifumixweb.appspot.com/template.html?duration=180&autologin=0&title=Afficher%20les%20messages%20partag%C3%A9s%20en%20mode%20g&widgets=message_cursor&public=false&noprompt=true&autorun=1&dest=undefined&ihm=perso&activities=message&flyer=https://shifumixweb.appspot.com/assets/img/ezgif.com-resize.gif";
+        if(template==8)url="https://shifumixweb.appspot.com/template.html?duration=60&autologin=0&title=Brainstorm&widgets=postit&public=false&noprompt=true&autorun=1&dest=PhotosPage&ihm=perso&activities=message&flyer=https://zeboute.files.wordpress.com/2018/06/fin_post_it_design_thinking_intelligence_collaborative.jpg";
+        if(template==9)url="https://shifumixweb.appspot.com/template.html?duration=480&autologin=0&title=My%20meeting&widgets=presentation&public=false&noprompt=true&autorun=1&dest=ChartsPage&ihm=perso&activities=presentation,message,survey&flyer=https://cdn.forbes.com.mx/2015/04/Internet-negocios-siluetas.jpg";
     } else {
         if(template==1)url="https://goo.gl/giM8HG";
         if(template==2)url="http://localhost:8080/widgets/freeScreen.html?eventcode=557394&sc=1547055085317&widget=musicOnly&event=event1547055085317&user=event_owner&message=Cette%20%C3%A9cran%20vous%20permet%20de%20partager%20votre%20%C3%A9v%C3%A9nement%20avec%20l%27ensemble%20des%20participants&1547055086050";
