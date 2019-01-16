e08087131ce716ac8b38539b7029e866d366d26b
shifumix <dev@shifumix.com>
2019-01-16 08:27:32 +0100

    avec les faq version 0.1

--- js/shifumix_website.js
+++ js/shifumix_website.js
@@ -18,14 +18,16 @@ function setprofil(i) {
 
     document.getElementById("iframe_tester").src = domain+"/tutoriel.html?screens=1&size=150&ihm=" + type_profil;
     document.getElementById("profil").classList.toggle("invisible");
-};
+}
+
+
 
 function openTemplates(){
     var add_url="";
     if(type_profil=="pro")add_url="?ihm=pro";
-    debugger
     window.open(domain+"/demo_template2.html"+add_url);
 }
+
 
 function openScreen(template){
     var url="";
@@ -38,14 +40,17 @@ function openScreen(template){
     window.open(url,"_blank","left=0,top=0,resizable=0,scrollbars=0,titlebar=0,status=0,width="+screen.availWidth+",height="+screen.availHeight);
 }
 
+
 function activaTab(tab){
     var elt=$('.faq-nav a[href="#' + tab + '"]');
     elt.tab('show');
 }
 
+
 $(document).ready(function() {
     if(location.href.indexOf("#")>-1){
         var param=location.href.split("#")[1];
+        if(param!=null)param=param.split("?")[0];
         var elt=document.getElementById(param);
         if(elt!=null){
             var section=elt.parentElement.parentElement.parentElement.id;
